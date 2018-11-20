7b22585c074dc711016be35725caac89ca1c6c16
Poine <poinix@gmail.com>
2018-11-20 17:41:40 +0100

    added multimaster in launch

--- oscar/oscar_control/src/oscar_hardware_interface.cpp
+++ oscar/oscar_control/src/oscar_hardware_interface.cpp
@@ -8,11 +8,11 @@ const std::string joint_name_[NB_JOINTS] = {"left_wheel_joint","right_wheel_join
 
 // Mechanics
 #define GEARBOX                       100.37
-#define ENCODER_RES                     12
+#define ENCODER_RES                    12
 #define WHEEL_RADIUS_M                  0.03
 // Electrical hookups
 #define STEERING_SERVO_CH               1
-#define STEERING_SERVO_NEUTRAL          0.11
+#define STEERING_SERVO_NEUTRAL         -0.11
 #define STEERING_SERVO_POLARITY        -1
 #define MOTOR_CHANNEL_L                 1
 #define MOTOR_CHANNEL_R                 2
